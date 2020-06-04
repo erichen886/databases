3b5728a397002f316a40c10910e3da08577ece79
erichen886 <erichen886@gmail.com>
2020-06-03 17:30:42 -0700

    database schema finished

--- server/schema.sql
+++ server/schema.sql
@@ -1,15 +1,31 @@
+DROP DATABASE IF EXISTS chat;
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  username VARCHAR(20) NOT NULL UNIQUE,
+  PRIMARY KEY(id)
+);
 
+CREATE TABLE rooms (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  roomname VARCHAR(20) NOT NULL UNIQUE,
+  PRIMARY KEY (id)
+);
 
-
+CREATE TABLE messages (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  messageText VARCHAR(200) NOT NULL,
+  usernameId INTEGER,
+  roomnameId INTEGER,
+  PRIMARY KEY (id),
+  FOREIGN KEY(usernameId) REFERENCES users(id),
+  FOREIGN KEY(roomnameId) REFERENCES rooms(id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
